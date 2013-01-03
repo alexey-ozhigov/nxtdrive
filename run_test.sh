0c08a43ca0a6b58f707b80a007d16f893eadb0c5
aozhigov <alexei.ozhigov@gmail.com>
2013-01-03 17:31:04 +0100

    Extended helper script.

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+#roslaunch cv_nxtdrive nxtdrive.launch depth:=true from_kinect:=true use_recognition:=false
 roslaunch cv_nxtdrive nxtdrive.launch depth:=true from_kinect:=true use_recognition:=true
